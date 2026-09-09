68bf986d346847c77491f3b88aefb3a892b1bec2
enyonyiabemba94-eng <enyonyiabemba94@gmail.com>
2026-09-09 22:30:54 +0300

    Update Android app entry point

--- android/app/src/main/java/com/kristumfalme/kwaya/MainActivity.java
+++ android/app/src/main/java/com/kristumfalme/kwaya/MainActivity.java
@@ -2,39 +2,30 @@ package com.kristumfalme.kwaya;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
     private WebView webView;
-    private static final String APP_URL = "https://enyonyiabemba94-eng.github.io/KWAYA-MANAGEMENT/?v=20260903-2222";
+    private static final String APP_URL = "https://enyonyiab94-eng.github.io/KWAYA-MANAGEMENT/?v=20260909-android";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         webView = new WebView(this);
         setContentView(webView);
-
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setDatabaseEnabled(true);
-        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        settings.setAppCacheEnabled(false);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
         settings.setSupportZoom(false);
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
-        settings.setMediaPlaybackRequiresUserGesture(false);
-
-        webView.clearCache(true);
-        webView.clearHistory();
-        webView.clearFormData();
         webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl(APP_URL);
     }
 
